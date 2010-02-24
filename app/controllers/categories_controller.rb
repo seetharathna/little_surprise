558ad279a203081e70b45d86630f65ef5421bc26
Isiri <isiri@isiri-desktop.(none)>
2010-02-24 12:17:38 +0530

    changes in categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -32,6 +32,7 @@ before_filter :check_admin,:except => :index #:set_current_user,
      @parent = Category.find_all_by_parent_id(nil)
      @wish_list = WishList.wishlist(facebook_session.user.uid) rescue nil
      
+     @fb_wish_list = WishList.find(:first,:conditions => ["facebook_id =?",facebook_session.user.to_i])
       
      if !params[:category_id].blank?
      @fb_categories = Category.find_all_by_parent_id(params[:category_id])
@@ -41,12 +42,12 @@ before_filter :check_admin,:except => :index #:set_current_user,
     @category_ids = []
     @category_ids = @wish_list.categories.collect { | h|  h.id } unless @wish_list.nil?
      respond_to do |format|
-       format.html # index.html.erb
+       format.html # index.html.erb 
        format.js {  render :update do |page|
                            page.replace_html 'category-list', :partial => 'list'
                        end
                   }
-       format.fbml #index.fbml.erb
+       format.fbml #{ ensure_authenticated_to_facebook  } #index.fbml.erb
      end
    end
 

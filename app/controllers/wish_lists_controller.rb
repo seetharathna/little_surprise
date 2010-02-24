85843d421bb20b9eedab8af20751744abca7dc95
Isiri <isiri@isiri-desktop.(none)>
2010-02-24 18:04:48 +0530

    wish_list creation

--- app/controllers/wish_lists_controller.rb
+++ app/controllers/wish_lists_controller.rb
@@ -72,7 +72,6 @@ class WishListsController < ApplicationController
   def publish_to_friends
     @wish_list = user.wish_list
     @category = Category.find(params[:category])
-    puts "ssssssssssssssssssssssssssssssssssssssssssss#{@category}"
     @user = facebook_session.user
     if @user.has_permissions?('publish_stream')
       facebook_session.user.publish_to(facebook_session.user, :message => 'has added new product categories to wishlist.',:action_links => [

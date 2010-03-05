dbc5053d3e82a7dc66a75a9e716e2733a0644f62
Isiri <isiri@isiri-desktop.(none)>
2010-03-05 17:01:49 +0530

    changes in categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -63,7 +63,7 @@ class CategoriesController < ApplicationController
       format.fbml {ensure_authenticated_to_facebook 
                     @current_user = user rescue nil
                     @wish_list = @current_user.wish_list unless @current_user.nil?
-                    @category = Category.find(params[:category_id]) rescue nil
+                    #@category = Category.find(params[:category_id]) rescue nil
                    
                     if !params[:category_id].blank?
                       link =[]

86c5ce953d66a02960ba0f73558187a97919943d
Isiri <isiri@isiri-desktop.(none)>
2010-03-05 16:59:42 +0530

    changes in categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -63,6 +63,19 @@ class CategoriesController < ApplicationController
       format.fbml {ensure_authenticated_to_facebook 
                     @current_user = user rescue nil
                     @wish_list = @current_user.wish_list unless @current_user.nil?
+                    @category = Category.find(params[:category_id]) rescue nil
+                   
+                    if !params[:category_id].blank?
+                      link =[]
+                      @link = Category.find(params[:category_id]) rescue nil             
+                      @fb_categories = Category.find_all_by_parent_id(params[:category_id])
+                      sub_categories = Category.find_all_by_parent_id(params[:category_id])
+                      if sub_categories.blank?
+                        redirect_to  category_path(params[:category_id])
+                      end
+                    else
+                      @fb_categories = Category.find_all_by_parent_id(nil)
+                    end
                     @category_ids = []
                      @category_ids = @wish_list.categories.collect { | h|  h.id } unless @wish_list.nil?
                     

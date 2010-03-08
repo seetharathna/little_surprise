e60d0ae004f2f923853fb676b4d7b817098b36c6
Isiri <isiri@isiri-desktop.(none)>
2010-03-08 16:03:01 +0530

    changes in categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,11 @@ class Category < ActiveRecord::Base
   #has_many :wish_lists,:through => :category_wish_lists
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "135x135>" }
   acts_as_tree :order => "name"
+
+
+  
+ def self.find_parent(category)
+    category = Category.find_by_parent_id(category.parent_id) rescue nil
+    return  category
+  end
 end

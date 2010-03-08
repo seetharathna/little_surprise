b6aa803a49e63486f4741f7674a7d2e550a60099
Isiri <isiri@isiri-desktop.(none)>
2010-03-08 14:52:56 +0530

    changes in categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -70,7 +70,7 @@ class CategoriesController < ApplicationController
                     if !params[:id].blank?
 
                       @links = [] 
-                       @links += Category.find_by_parent_id(params[:links]) if params[:links]                  
+                       @links += params[:links] if params[:links]                  
                        @links << Category.find(params[:id]).id if params[:id]
                      
                       @fb_categories = Category.find_all_by_parent_id(params[:id])
